bf0d9acc59337431c3b7ce6c3e27f863cea446ca
LakshyaDuck <163497856+LakshyaDuck@users.noreply.github.com>
2026-03-14 02:17:32 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,7 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -21,12 +21,10 @@ app = FastAPI(title="Airplane E‑Ticketing API", lifespan=lifespan, redirect_sl
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",  # Vite dev server
-        "http://127.0.0.1:5173",
-        "http://localhost:3000",  # If you have other frontend ports
-        # OR use "*" for development (not recommended for production)
-        "https://69b46b787cf2093cb63e2a43--gleaming-salamander-d885e4.netlify.app"
-    ],
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://gleaming-salamander-d885e4.netlify.app"
+],
     allow_credentials=True,
     allow_methods=["*"],  # Allows all HTTP methods (GET, POST, PUT, DELETE, etc.)
     allow_headers=["*"],  # Allows all headers
